9b3245b443f4e3d54959d0377d2a37dc44ef1a7a
Robert Machmer <robert.machmer@gmail.com>
2015-04-24 20:14:50 +0200

    Add ImageDecorator class

---
+++ ui/decorators/ImageDecorator.lua
@@ -0,0 +1,17 @@
+local BaseDecorator = require('ui.decorators.BaseDecorator');
+
+local ImageDecorator = {};
+
+function ImageDecorator.new(img, x, y, w, h)
+    local self = BaseDecorator.new();
+
+    function self:draw()
+        self.super:draw();
+        local px, py = self:getPosition();
+        love.graphics.draw(img, px + x, py + y, 0, w / img:getWidth(), h / img:getHeight());
+    end
+
+    return self;
+end
+
+return ImageDecorator;
