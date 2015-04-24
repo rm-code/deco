3c5a96215a87ca56d6b388a66dbdc6202423a174
Robert Machmer <robert.machmer@gmail.com>
2015-04-24 19:22:22 +0200

    Add BoxDecorator class

---
+++ ui/decorators/BoxDecorator.lua
@@ -0,0 +1,39 @@
+local BaseDecorator = require('ui.decorators.BaseDecorator');
+
+local BoxDecorator = {};
+
+function BoxDecorator.new(baseCol, highlightCol, x, y, w, h)
+    local self = BaseDecorator.new();
+
+    local currentCol = baseCol;
+
+    function self:draw()
+        self.super:draw();
+        local px, py = self.super:getPosition();
+        love.graphics.setColor(currentCol);
+        love.graphics.rectangle('fill', px + x, py + y, w, h)
+        love.graphics.setColor(255, 255, 255, 100);
+        love.graphics.rectangle('line', px + x, py + y, w, h)
+        love.graphics.setColor(255, 255, 255, 255);
+    end
+
+    function self:update(dt)
+        self:intersects(love.mouse.getPosition());
+    end
+
+    function self:intersects(cx, cy)
+        local px, py = self.super:getPosition();
+
+        if px + x < cx and px + x + w > cx and py + y < cy and py + h > cy then
+            currentCol = highlightCol;
+            return true;
+        else
+            currentCol = baseCol;
+            return self.super:intersects(cx, cy);
+        end
+    end
+
+    return self;
+end
+
+return BoxDecorator;
