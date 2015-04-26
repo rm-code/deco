48b5592bb0d9231de0e57de3719002252b3d28a2
Robert Machmer <robert.machmer@gmail.com>
2015-04-26 11:47:10 +0200

    Slim down BoxDecorator class
    Removed code which didn't really fit the area of responsibility of a
    BoxDecorator. Now, it only takes care of drawing a simple box, either in
    "fill" or in "line" mode.

--- ui/decorators/BoxDecorator.lua
+++ ui/decorators/BoxDecorator.lua
@@ -2,35 +2,15 @@ local BaseDecorator = require('ui.decorators.BaseDecorator');
 
 local BoxDecorator = {};
 
-function BoxDecorator.new(baseCol, highlightCol, outlineCol, x, y, w, h)
+function BoxDecorator.new(mode, rgba, x, y, w, h)
     local self = BaseDecorator.new();
-
-    local currentCol = baseCol;
 
     function self:draw()
         self.child:draw();
         local px, py = self.child:getPosition();
-        love.graphics.setColor(currentCol);
-        love.graphics.rectangle('fill', px + x, py + y, w, h)
-        love.graphics.setColor(outlineCol);
-        love.graphics.rectangle('line', px + x, py + y, w, h)
+        love.graphics.setColor(rgba);
+        love.graphics.rectangle(mode, px + x, py + y, w, h)
         love.graphics.setColor(255, 255, 255, 255);
-    end
-
-    function self:update(dt)
-        self:intersects(love.mouse.getPosition());
-    end
-
-    function self:intersects(cx, cy)
-        local px, py = self.child:getPosition();
-
-        if px + x < cx and px + x + w > cx and py + y < cy and py + y + h > cy then
-            currentCol = highlightCol;
-            return true;
-        else
-            currentCol = baseCol;
-            return self.child:intersects(cx, cy);
-        end
     end
 
     return self;
