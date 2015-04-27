1ed1a9e76c1d25994dfb97b144ff22bafc48659a
Robert Machmer <robert.machmer@gmail.com>
2015-04-27 13:49:56 +0200

    ClickableDecorator class now takes a command object
    This object should have an execute method which will be called when a
    click happens.

--- ui/decorators/ClickableDecorator.lua
+++ ui/decorators/ClickableDecorator.lua
@@ -2,13 +2,13 @@ local BaseDecorator = require('ui.decorators.BaseDecorator');
 
 local ClickableDecorator = {};
 
-function ClickableDecorator.new(func, x, y, w, h)
+function ClickableDecorator.new(command, x, y, w, h)
     local self = BaseDecorator.new();
 
     function self:mousepressed(mx, my, b)
         local px, py = self.child:getPosition();
         if px + x < mx and px + x + w > mx and py + y < my and py + y + h > my then
-            func();
+            command:execute();
         else
             self.child:mousepressed(mx, my, b);
         end
