1442ec1ae5045c290366e67360b4074d589ae0c0
Robert Machmer <robert.machmer@gmail.com>
2015-04-29 16:53:04 +0200

    Fix ScrollableDecorator not checking for mouse focus
    It now checks if the mouse is over the decorator before scrolling.

--- ui/decorators/ScrollableDecorator.lua
+++ ui/decorators/ScrollableDecorator.lua
@@ -6,15 +6,19 @@ function ScrollableDecorator.new(x, y, w, h)
     local self = BaseDecorator.new();
 
     function self:mousepressed(mx, my, b)
-        if b == 'wu' then
-            local ox, oy = self.child:getContentOffset();
-            self.child:setContentOffset(ox, oy - 4);
-        elseif b == 'wd' then
-            local ox, oy = self.child:getContentOffset();
-            self.child:setContentOffset(ox, oy + 4);
-        else
-            self.child:mousepressed(mx, my, b);
+        local px, py = self:getPosition();
+        local pw, ph = self:getDimensions();
+
+        if px + x < mx and px + x + pw + w > mx and py + y < my and py + y + ph + h > my then
+            if b == 'wu' then
+                local ox, oy = self.child:getContentOffset();
+                self.child:setContentOffset(ox, oy - 4);
+            elseif b == 'wd' then
+                local ox, oy = self.child:getContentOffset();
+                self.child:setContentOffset(ox, oy + 4);
+            end
         end
+        self.child:mousepressed(mx, my, b);
     end
 
     return self;
