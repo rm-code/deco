8229d3d0983862d6f883aae664259252da4f4a91
Robert Machmer <robert.machmer@gmail.com>
2015-04-29 16:39:45 +0200

    Simplify TextLabel class
    It now uses love.graphics.print instead of printf. This decorator should
    only be used for simple text displaying. For more elaborate control the
    TextArea class should be used.

--- ui/decorators/TextLabel.lua
+++ ui/decorators/TextLabel.lua
@@ -2,16 +2,15 @@ local BaseDecorator = require('ui.decorators.BaseDecorator');
 
 local TextLabel = {};
 
-function TextLabel.new(text, rgba, font, x, y, w)
+function TextLabel.new(text, rgba, font, x, y)
     local self = BaseDecorator.new();
 
     function self:draw()
         self.child:draw();
         local px, py = self:getPosition();
-        local pw, ph = self:getDimensions();
         love.graphics.setFont(font);
         love.graphics.setColor(rgba);
-        love.graphics.printf(text, px + x, py + y, pw + w);
+        love.graphics.print(text, px + x, py + y);
         love.graphics.setColor(255, 255, 255, 255);
     end
 
