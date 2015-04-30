9620a6a6e357cf8989d7c5b1fb085a04450a20ee
Robert Machmer <robert.machmer@gmail.com>
2015-04-30 16:03:09 +0200

    Add smooth scrolling to ScrollableDecorator class
    It now adds a velocity which is reduced over time instead of changing
    the position of the content directly.

--- ui/decorators/ScrollableDecorator.lua
+++ ui/decorators/ScrollableDecorator.lua
@@ -2,22 +2,50 @@ local BaseDecorator = require('ui.decorators.BaseDecorator');
 
 local ScrollableDecorator = {};
 
+local MAX_VELOCITY = 8;
+local SCROLL_SPEED = 2;
+local DAMPING = 8;
+
 function ScrollableDecorator.new(x, y, w, h, anchorHor, anchorVer)
     local self = BaseDecorator.new();
+
+    local scrollVelocity = 0;
+
+    function self:update(dt)
+        -- Reduce the scrolling velocity over time.
+        if scrollVelocity < -0.5 then
+            scrollVelocity = scrollVelocity + dt * DAMPING;
+        elseif scrollVelocity > 0.5 then
+            scrollVelocity = scrollVelocity - dt * DAMPING;
+        else
+            scrollVelocity = 0;
+        end
+
+        -- Clamp velocity to prevent too fast scrolling.
+        scrollVelocity = math.max(-MAX_VELOCITY, math.min(scrollVelocity, MAX_VELOCITY));
+
+        -- Update the position of the scrolled content.
+        local ox, oy = self.child:getContentOffset();
+        self.child:setContentOffset(ox, oy + scrollVelocity);
+
+        self.child:update(dt);
+    end
 
     function self:mousepressed(mx, my, b)
         local px, py = self:getPosition();
         local pw, ph = self:getDimensions();
 
+        -- Check if the mousepointer is over the scroll panel before applying scroll.
         if px + x < mx and px + x + pw + w > mx and py + y < my and py + y + ph + h > my then
             if b == 'wu' then
-                local ox, oy = self.child:getContentOffset();
-                self.child:setContentOffset(ox, oy - 4);
+                scrollVelocity = scrollVelocity > 0 and 0 or scrollVelocity;
+                scrollVelocity = scrollVelocity - SCROLL_SPEED;
             elseif b == 'wd' then
-                local ox, oy = self.child:getContentOffset();
-                self.child:setContentOffset(ox, oy + 4);
+                scrollVelocity = scrollVelocity < 0 and 0 or scrollVelocity;
+                scrollVelocity = scrollVelocity + SCROLL_SPEED;
             end
         end
+
         self.child:mousepressed(mx, my, b);
     end
 
